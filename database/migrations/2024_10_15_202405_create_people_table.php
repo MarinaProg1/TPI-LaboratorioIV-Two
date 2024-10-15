691b6a7e164bfe6113690a8ea237e655039e66ff
MarinaProg1 <153437784+MarinaProg1@users.noreply.github.com>
2024-10-15 19:43:29 -0300

    cambios temporales

--- database/migrations/2024_10_15_202405_create_people_table.php
+++ database/migrations/2024_10_15_202405_create_people_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
-            $table->string(first_name);
-            $table->string(document_type)
+            $table->string('first_name');
+            $table->string('document_type');
             $table->timestamps();
         });
     }
